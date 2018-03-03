fb884dfabff2894cbebe06f4e9150f05c594b3a7
James Noble <kjx@ecs.vuw.ac.nz>
2018-03-04 12:56:37 +1300

    Redirect error for tests

--- run-test.sh
+++ run-test.sh
@@ -2,5 +2,5 @@
 
 KERNAN='mono --debug /Users/kjx/mwh-gre/kernan/Grace/bin/Debug/Grace.exe'
 
-$KERNAN main.grace test/test.grace | sed 's/\[[A-Z0-9]*\]/[-]/g' > test/OUT-test.txt
+$KERNAN main.grace test/test.grace | sed 's/\[[A-Z0-9]*\]/[-]/g' &> test/OUT-test.txt
 diff test/EXP-test.txt test/OUT-test.txt
