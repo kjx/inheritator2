1fd032e1ebc2bc15a8590069feeeb6838fb24b3f
James Noble <kjx@ecs.vuw.ac.nz>
2018-08-02 07:38:29 +1200

    Add brand-messages to run-tests.sh

--- run-test.sh
+++ run-test.sh
@@ -4,3 +4,7 @@ KERNAN='mono --debug /Users/kjx/mwh-gre/kernan/Grace/bin/Debug/Grace.exe'
 
 $KERNAN main.grace test/test.grace | sed 's/\[[A-Z0-9]*\]/[-]/g' &> test/OUT-test.txt
 diff test/EXP-test.txt test/OUT-test.txt
+
+$KERNAN main.grace test/brand-messages.grace 2>&1 | sed 's/\[[A-Z0-9]*\]/[-]/g' &> test/OUT-brand-messages.txt
+
+diff test/EXP-brand-messages.txt test/OUT-brand-messages.txt
